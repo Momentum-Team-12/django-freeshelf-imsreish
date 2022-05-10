472f57cb5f0f19cb286097b266731a876cbfe3bf
Stephen Reish <spreish@ncsu.edu>
2022-05-10 11:37:13 -0400

    Some progress in associated categories

--- books/models.py
+++ books/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.template.defaultfilters import slugify
+from django.urls import reverse
 
 
 class User(AbstractUser):
@@ -14,5 +16,25 @@ class Book(models.Model):
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=255)
     description = models.TextField(null=True, blank=True)
+    # in this implementation, the book can have only one category
+    category = models.ForeignKey('Category', related_name='books', on_delete=models.CASCADE, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(null=False, unique=True)
+
+    def get_absolute_url(self):
+        return reverse('category_detail', kwargs={'slug': self.slug})
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        return super().save(*args, **kwargs)
+
+
+class Favorite(models.Model):
+    book = models.ForeignKey('Book', related_name='favorites', on_delete=models.CASCADE)
+    user = models.ForeignKey('User', related_name='favorites', on_delete=models.CASCADE)
